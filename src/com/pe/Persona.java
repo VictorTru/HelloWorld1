48072f5927179fc6d52e0c53cab867d59f63bc02
Victor Trujillo <vtrujillocharalla@gmail.com>
2022-12-13 01:47:26 -0500

    Se reemplaza clase Persona por registro Persona

--- src/com/pe/Persona.java
+++ src/com/pe/Persona.java
@@ -1,36 +1,10 @@
 package com.pe;
 
-public class Persona {
-
-    private String nombre,apellidos;
-
-    public Persona(String nombre, String apellidos) {
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
+public record Persona(String nombre, String apellidos) {
 
     @Override
     public String toString() {
-        /*return "Persona{" +
-                "nombre='" + nombre + '\'' +
-                ", apellidos='" + apellidos + '\'' +
-                '}';*/
         return "%s %s".formatted(nombre,apellidos);
     }
+
 }
