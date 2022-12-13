d53942ee1c0d3808aa82ee2d0131c51eded444ef
Victor Trujillo <vtrujillocharalla@gmail.com>
2022-12-13 00:56:41 -0500

    Se quito la linea de codigo Inservible

--- src/com/pe/PersonaRegistro.java
+++ src/com/pe/PersonaRegistro.java
@@ -4,8 +4,6 @@ public record PersonaRegistro(String nombre, String apellidos) {
 
     @Override
     public String toString() {
-        // Esta linea ya esta revisada
-        //System.out.println("Esta instruccion es inservible");
         return "%s %s".formatted(nombre,apellidos);
     }
 
